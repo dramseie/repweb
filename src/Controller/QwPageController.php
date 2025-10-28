42614c5d7b1ee40dca01f584459c7ee7989af77d
David Ramseier <daivd@ramseier.com>
2025-10-28 14:39:49 +0100

    Add root route /qw/builder -> redirect to /qw/builder/1 (QwPageController)

--- src/Controller/QwPageController.php
+++ src/Controller/QwPageController.php
@@ -9,6 +9,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 final class QwPageController extends AbstractController
 {
+    #[Route('/qw/builder', name: 'qw_builder_root')]
+    public function index(): Response
+    {
+        // redirect to a default questionnaire id (1) so the builder is reachable
+        return $this->redirectToRoute('qw_builder', ['id' => 1]);
+    }
+
     #[Route('/qw/builder/{id}', name: 'qw_builder')]
     public function builder(int $id): Response
     {
