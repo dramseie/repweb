2b77c7732b2b8231acb06ca68e089658dec22b8b
David Ramseier <daivd@ramseier.com>
2026-02-05 19:36:08 +0100

    Automated repweb build

--- src/Controller/Api/SmartsheetPresentationController.php
+++ src/Controller/Api/SmartsheetPresentationController.php
@@ -539,7 +539,7 @@ class SmartsheetPresentationController extends AbstractController
 
         $dompdf = new Dompdf($options);
         $dompdf->loadHtml($html, 'UTF-8');
-        $dompdf->setPaper('A3', 'landscape');
+        $dompdf->setPaper('A2', 'landscape');
         $dompdf->render();
 
         $pdfOutput = $dompdf->output();
