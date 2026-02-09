cb3a450f5e8b7bf66e2e62c656a321a83c56feb1
David Ramseier <daivd@ramseier.com>
2026-02-09 12:20:39 +0100

    Automated repweb build

--- src/Controller/Api/SmartsheetPresentationController.php
+++ src/Controller/Api/SmartsheetPresentationController.php
@@ -1720,8 +1720,8 @@ class SmartsheetPresentationController extends AbstractController
         }
 
         $country = trim((string) $request->query->get('country', ''));
-        $sitesParam = $request->query->all('sites');
-        $tasksParam = $request->query->all('tasks');
+        $sitesParam = $request->query->get('sites', []);
+        $tasksParam = $request->query->get('tasks', []);
         $sites = is_array($sitesParam) ? $sitesParam : (is_string($sitesParam) && $sitesParam !== '' ? [$sitesParam] : []);
         $tasks = is_array($tasksParam) ? $tasksParam : (is_string($tasksParam) && $tasksParam !== '' ? [$tasksParam] : []);
         $from = trim((string) $request->query->get('from', ''));
