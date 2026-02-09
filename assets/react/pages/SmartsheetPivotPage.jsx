a49ba502dd5bfac47df1f0138bbe1c8ab52cd414
David Ramseier <daivd@ramseier.com>
2026-02-09 18:21:15 +0100

    Automated repweb build

--- assets/react/pages/SmartsheetPivotPage.jsx
+++ assets/react/pages/SmartsheetPivotPage.jsx
@@ -6057,7 +6057,7 @@ const SmartsheetPivotPage = () => {
 
                   <div className="row g-3 align-items-end">
                     <div className="col-12 col-md-3">
-                      <label className="form-label fw-medium">Baseline date<br /></label>
+                      <label className="form-label fw-medium">Baseline date</label>
                       <DatePicker
                         selected={trendBaselineDate}
                         onChange={(date) => setTrendBaselineDate(date || new Date())}
