0e2ea9f10e5521b43e4aa854c722ac6eb078487e
David Ramseier <daivd@ramseier.com>
2026-02-10 20:38:19 +0100

    Automated repweb build

--- src/Controller/Api/SmartsheetPresentationController.php
+++ src/Controller/Api/SmartsheetPresentationController.php
@@ -1677,7 +1677,7 @@ class SmartsheetPresentationController extends AbstractController
     #[Route('/duration-analysis', name: 'presentation_duration_analysis', methods: ['GET'])]
     public function durationAnalysis(): JsonResponse
     {
-        $stmt = $this->connection->executeQuery('CALL sp_smartsheet_site_task_duration_pivot()');
+        $stmt = $this->connection->executeQuery('CALL nifi.sp_smartsheet_site_task_duration_pivot()');
         $rows = $stmt->fetchAllAssociative();
         $stmt->free();
 
